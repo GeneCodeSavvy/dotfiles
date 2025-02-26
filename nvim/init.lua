3d46b00d0a41136d9d5158a14ca3544fb04fe471
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-02-26 20:47:01 +0530

    cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 require("vim-options")
 require("keymaps")
-require('auotcmd')
+require('autocmd')
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
