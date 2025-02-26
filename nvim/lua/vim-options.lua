73d67eaaa00a3ca047a2628a434b142eec424137
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-02-26 17:43:46 +0530

    vim-options

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,20 +1,61 @@
+-- Set <space> as the local leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
+-- Make line numbers default
 vim.opt.relativenumber = true
 vim.opt.number = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = "a"
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
 vim.opt.scrollbind = true
 vim.opt.wrap = false
 vim.opt.spelllang = "en"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.scrolloff = 999
 vim.opt.virtualedit = "block"
-vim.opt.inccommand = "split"
 vim.opt.termguicolors = true
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
 vim.opt.signcolumn = "yes"
 
-vim.g.mapleader = " "
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
