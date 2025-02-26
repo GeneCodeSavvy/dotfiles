b238fb5cbac6088d9d77466c3688f18afaed3bf7
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-02-26 20:45:27 +0530

    which-key and cleanup

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,8 @@
-	-- Here is a more advanced example where we pass configuration
-	-- options to `gitsigns.nvim`.
-	--
-	-- See `:help gitsigns` to understand what the configuration keys do
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
+-- Here is a more advanced example where we pass configuration
+-- options to `gitsigns.nvim`.
+--
+-- See `:help gitsigns` to understand what the configuration keys do
+-- Adds git related signs to the gutter, as well as utilities for managing changes
 return {     
     "lewis6991/gitsigns.nvim",
     opts = {
@@ -14,4 +14,4 @@ return {
             changedelete = { text = "~" },
         },
     },
-},
+}
