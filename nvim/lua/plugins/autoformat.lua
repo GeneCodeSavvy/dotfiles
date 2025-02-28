87dd8dd42d14e3930eb97de0d9d1a6dfdec78433
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-02-28 11:00:31 +0530

    isort installed

--- nvim/lua/plugins/autoformat.lua
+++ nvim/lua/plugins/autoformat.lua
@@ -33,6 +33,7 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			-- Conform can also run multiple formatters sequentially
+			python = { "isort" },
 			-- python = { "isort", "black" },
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
