f3efb0513c9408467335386b4b32576473bdc589
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-07-14 23:45:50 +0530

    install fork-you-dev plugin for neovim

---
+++ nvim/lua/plugins/fork-you-dev.lua
@@ -0,0 +1,8 @@
+return {
+  "forkyoudev/forkyou.nvim",
+  config = function()
+    require("forkyou").setup({
+      api_token = "60cb1bc0-0e01-4fb2-a725-42ef4055e291", -- Required
+    })
+  end,
+}
