a743332b4a2dd01ff384fc953df85e75957034b7
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-08-02 13:12:59 +0530

    remove forkyoudev

--- nvim/lua/plugins/fork-you-dev.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "forkyoudev/forkyou.nvim",
-  config = function()
-    require("forkyou").setup({
-      api_token = "60cb1bc0-0e01-4fb2-a725-42ef4055e291", -- Required
-    })
-  end,
-}
