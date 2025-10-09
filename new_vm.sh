52b9817825b9a6ff33fafd35268d997045a3a924
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-10-10 03:26:21 +0530

    install go and setup caddy

--- new_vm.sh
+++ new_vm.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # ghostty clear command
 echo 'export TERM=xterm-256color' >> ~/.bashrc
 
@@ -29,9 +31,34 @@ nvm alias default node
 # setting up python
 sudo apt install python3-pip python3-venv -y
 
-# installing and setting up go
-sudo apt install golang-go -y
-echo "export PATH=$PATH:$(go env GOPATH)/bin" >> ~/.bashrc
+# installing and setting up Go from source, including necessary bootstrap.
+## download and set up the bootstrap compiler
+cd ~
+## download the specific, known-working bootstrap version for arm64
+wget https://go.dev/dl/go1.25.2.linux-arm64.tar.gz
+## extract the bootstrap files and rename the directory
+sudo tar -C . -xzf go1.25.2.linux-arm64.tar.gz
+mv go go-bootstrap
+## set the environment variable for the build process
+export GOROOT_BOOTSTRAP=$HOME/go-bootstrap
+## get the Go source code
+cd /usr/local/
+## Optional cleanup of any previous installation
+sudo rm -rf go 
+## Clone the latest Go source code
+sudo git clone https://go.googlesource.com/go
+cd go/src/
+git checkout master
+## use the downloaded bootstrap to compile the new toolchain
+sudo ./make.bash
+# set the PATH for the current user and session
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> ~/.bashrc
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+# Clean up the bootstrap files
+cd ~
+sudo rm -rf go-bootstrap go1.25.2.linux-arm64.tar.gz
+# install a standard Go tool (gopls)
+go install golang.org/x/tools/gopls@latest
 
 # setting up config files
 cd ~
@@ -44,6 +71,8 @@ eval "$STARTUP_COMMAND"
 
 # setting up xcaddy, for caddy custom builds
 go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+xcaddy build --with github.com/mholt/caddy-ratelimit
+sudo mv caddy /usr/local/bin/
 
 # reboot the system
 sudo reboot
